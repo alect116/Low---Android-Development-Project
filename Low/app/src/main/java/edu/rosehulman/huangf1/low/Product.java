3800873b439bc6937d848cdf58f6e464bd855fa6
huangf1 <huangf1@rose-hulman.edu>
2017-01-15 22:40:10 -0500

    Product

--- Low/app/src/main/java/edu/rosehulman/huangf1/low/Product.java
+++ Low/app/src/main/java/edu/rosehulman/huangf1/low/Product.java
@@ -5,4 +5,43 @@ package edu.rosehulman.huangf1.low;
  */
 
 public class Product {
+    private String mName;
+    private String mRetailer;
+    private String mLink;
+    private Double mPrice;
+
+    public Product() {
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmRetailer() {
+        return mRetailer;
+    }
+
+    public void setmRetailer(String mRetailer) {
+        this.mRetailer = mRetailer;
+    }
+
+    public String getmLink() {
+        return mLink;
+    }
+
+    public void setmLink(String mLink) {
+        this.mLink = mLink;
+    }
+
+    public Double getmPrice() {
+        return mPrice;
+    }
+
+    public void setmPrice(Double mPrice) {
+        this.mPrice = mPrice;
+    }
 }
