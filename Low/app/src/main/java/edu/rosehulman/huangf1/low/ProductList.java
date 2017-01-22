61d55526e50956c4382ff919916d50b25af87798
alect116 <thompsar@rose-hulman.edu>
2017-01-22 18:00:15 -0500

    small changes

--- Low/app/src/main/java/edu/rosehulman/huangf1/low/ProductList.java
+++ Low/app/src/main/java/edu/rosehulman/huangf1/low/ProductList.java
@@ -1,9 +1,17 @@
 package edu.rosehulman.huangf1.low;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.SearchView;
 import android.widget.EditText;
 
@@ -35,7 +43,6 @@ public class ProductList extends AppCompatActivity {
         listView.setLayoutManager(new GridLayoutManager(this, 2));
         listView.setAdapter(mAdapter);
         listView.setHasFixedSize(true);
-
 
 
 
